e6875c278d6d0de81e29a67b8905ebe131fc0620
wen <Wen_Cheng@wistron.com>
2025-11-27 09:33:04 +0800

    [#8] feat: 更新 User model 添加 UserStatus 枚舉

--- internal/database/model/user.go
+++ internal/database/model/user.go
@@ -2,12 +2,22 @@ package model
 
 import "time"
 
+type UserStatus string
+
+const (
+	UserStatusApplying  UserStatus = "APPLYING"
+	UserStatusActivated UserStatus = "ACTIVATED"
+	UserStatusDisabled  UserStatus = "DISABLED"
+)
+
 type User struct {
-	ID        uint      `gorm:"primary_key;column:id"`
-	Name      string    `gorm:"column:name;not null"`
-	Account   string    `gorm:"column:account;not null"`
-	Password  string    `gorm:"column:password;not null"`
-	CreatedAt time.Time `gorm:"column:created_at"`
+	ID         int64      `db:"id" json:"id"`
+	Name       string     `db:"name" json:"name"`
+	Email      string     `db:"email" json:"email"`
+	Password   string     `db:"password" json:"-"` // 不回傳密碼
+	UserStatus UserStatus `db:"user_status" json:"user_status"`
+	UserIP     string     `db:"user_ip" json:"user_ip"`
+	CreatedAt  time.Time  `db:"created_at" json:"created_at"`
 }
 
 func (User) TableName() string {
