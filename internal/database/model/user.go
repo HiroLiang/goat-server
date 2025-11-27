7fb96eb13f31f143246da2b6b1843bd9f30ae13c
wen <Wen_Cheng@wistron.com>
2025-11-27 09:33:04 +0800

    [#8] feat: 更新 User model 添加 UserStatus 枚舉

--- internal/database/model/user.go
+++ internal/database/model/user.go
@@ -2,28 +2,22 @@ package model
 
 import "time"
 
-/*
-type User struct {
-	ID        uint      `gorm:"primary_key;column:id"`
-	Name      string    `gorm:"column:name;not null"`
-	Account   string    `gorm:"column:account;not null"`
-	Password  string    `gorm:"column:password;not null"`
-	CreatedAt time.Time `gorm:"column:created_at"`
-}
+type UserStatus string
 
-func (User) TableName() string {
-	return "public.users"
-}
-*/
+const (
+	UserStatusApplying  UserStatus = "APPLYING"
+	UserStatusActivated UserStatus = "ACTIVATED"
+	UserStatusDisabled  UserStatus = "DISABLED"
+)
 
 type User struct {
-	ID         int64     `db:"id" gorm:"primary_key;column:id"`
-	Name       string    `db:"name" gorm:"column:name;not null"`
-	Email      string    `db:"email" gorm:"column:email;not null"`
-	Password   string    `db:"password" gorm:"column:password;not null"`
-	UserStatus string    `db:"user_status" gorm:"column:user_status;not null"`
-	UserIP     string    `db:"user_ip" gorm:"column:user_ip;not null"`
-	CreatedAt  time.Time `db:"created_at" gorm:"column:created_at"`
+	ID         int64      `db:"id" json:"id"`
+	Name       string     `db:"name" json:"name"`
+	Email      string     `db:"email" json:"email"`
+	Password   string     `db:"password" json:"-"` // 不回傳密碼
+	UserStatus UserStatus `db:"user_status" json:"user_status"`
+	UserIP     string     `db:"user_ip" json:"user_ip"`
+	CreatedAt  time.Time  `db:"created_at" json:"created_at"`
 }
 
 func (User) TableName() string {
